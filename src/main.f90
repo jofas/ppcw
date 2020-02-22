1a234f8511b4db4e0770d47c1a4a9b3f175456bf
jofas <jonas@fc-web.de>
2020-02-22 17:50:23 +0000

    beautified code

--- src/main.f90
+++ src/main.f90
@@ -1,271 +1,260 @@
 program main
-  !
-  ! Control program for the MD update
-  !
-
   implicit none
 
-! This file defines a common block that contains the primary coordinates
-! of the particles,
-!
-!  Nbody	    Number of particles
-!  Npair    	Number of particle pairs
-!  pos		    Position of the particles
-!  r            Distance of particle from central mass
-!  velo		    velocity of the particles
-!  f		    Forces acting on each particle
-!  vis		    viscosity coefficient for each particle
-!  mass		    mass of each particle
-!  delta_pos	separation vector for each particle pair
-!  delta_r		separation for each particle pair
-!
-      INTEGER Nbody, Npair
-      PARAMETER( Nbody=4*1024, Npair=(Nbody*(Nbody-1))/2 )
-      INTEGER Xcoord, Ycoord, Zcoord, Ndim
-      PARAMETER( Xcoord=1, Ycoord=2, Zcoord=3, Ndim=3 )
-      DOUBLE PRECISION r(Nbody)
-      DOUBLE PRECISION pos(Nbody,Ndim), velo(Nbody,Ndim)
-      DOUBLE PRECISION f(Nbody,Ndim), vis(Nbody), mass(Nbody)
-      DOUBLE PRECISION radius(Nbody)
-      DOUBLE PRECISION delta_pos(Nbody*Nbody,Ndim)
-      DOUBLE PRECISION delta_r(Nbody*Nbody)
-      DOUBLE PRECISION wind(Ndim)
-      INTEGER collisions
+  !
+  ! Nbody	    number of particles
+  ! Npair    	number of particle pairs
+  ! pos		    position of the particles
+  ! r         distance of particle from central mass
+  ! velo		  velocity of the particles
+  ! f		      forces acting on each particle
+  ! vis		    viscosity coefficient for each particle
+  ! mass		  mass of each particle
+  ! delta_pos	separation vector for each particle pair
+  ! delta_r		separation for each particle pair
+  !
+  integer, parameter :: Nbody = 4 * 1024, Ndim = 3
+  integer :: Npair = (Nbody * (Nbody - 1)) / 2
+  integer :: Xcoord = 1, Ycoord = 2, Zcoord = 3
 
-!      COMMON /coord/ f, pos, vis, mass, radius, velo, &
-!              delta_pos, delta_r, &
-!              r,collisions,wind
+  double precision, dimension(Nbody) :: r, vis, mass, radius
+  double precision, dimension(Nbody, Ndim) :: f, pos, velo
+  double precision, dimension(Nbody * Nbody, Ndim) :: delta_pos
+  double precision, dimension(Nbody * Nbody) :: delta_r
+  double precision, dimension(Ndim) :: wind
+  integer :: collisions
+
+  ! size of central mass
+  double precision :: M_central = 1000d0
+
+  ! interaction strength for pairwise interactions
+  double precision :: G = 2d0
 
 
-!
-! Size of central mass.
-!
-      DOUBLE PRECISION M_central
-      PARAMETER( M_central = 1000d0)
+  integer :: i, j
+  integer :: clock_rate, clock_max
+  integer :: tstart, tstop
+  integer :: istart, istop
 
-!
-! Interaction strength for pairwise interactions.
-!
-      DOUBLE PRECISION G
-      PARAMETER( G = 2d0 )
-  !INCLUDE 'coord.inc'
+  ! timestep value
+  double precision :: dt = 0.02
 
+  ! number of timesteps to use
+  integer :: Nstep = 100, Nsave = 5
 
-  INTEGER i,j
-  INTEGER clock_rate, clock_max
-  INTEGER tstart, tstop
-  INTEGER istart, istop
-
-  !  timestep value
-  DOUBLE PRECISION :: dt = 0.02
-  !PARAMETER( dt = 0.02 )
-
-  !  number of timesteps to use.
-  INTEGER :: Nstep = 100, Nsave = 5
-  !    INTEGER Nsave
-  !PARAMETER(Nstep=100,Nsave=5)
-  CHARACTER(len = 80) :: file_out
-
-  !read the initial data from a file
+  character(len = 80) :: file_out
 
   wind(XCoord) = 0.9
   wind(YCoord) = 0.4
   wind(ZCoord) = 0.0
+
   collisions=0
 
-  OPEN(unit=1,file='input.dat',status='OLD')
-  DO i=1,Nbody
-    READ(1,11) mass(i),vis(i), &
-      pos(i,Xcoord),pos(i,Ycoord),pos(i,Zcoord), &
-      velo(i,Xcoord),velo(i,Ycoord),velo(i,Zcoord)
-    radius(i)=0.5
-  END DO
-  CLOSE(unit=1)
+  !read the initial data from a file
+  open(unit=1, file='input.dat', status='OLD')
+  do i = 1, Nbody
 
-  !
-  ! Run 20 timesteps and time how long it took
-  !
+    read(1, 11) mass(i), vis(i), &
+      pos (i, Xcoord), pos (i, Ycoord), pos (i, Zcoord), &
+      velo(i, Xcoord), velo(i, Ycoord), velo(i, Zcoord)
 
-  CALL SYSTEM_CLOCK(tstart,clock_rate,clock_max)
-  DO j=1,Nsave
-    CALL SYSTEM_CLOCK(istart,clock_rate,clock_max)
-    CALL evolve(Nstep,dt)
-    CALL SYSTEM_CLOCK(istop,clock_rate,clock_max)
+    ! TODO: radius to constant
+    radius(i) = 0.5
+  end do
+  close(unit=1)
 
-    WRITE(*,*) (Nstep), ' timesteps took ', &
-      REAL(istop-istart)/REAL(clock_rate)
-    write(*,*) collisions, ' collisions'
 
+  call system_clock(tstart, clock_rate, clock_max)
+
+  do j = 1, Nsave
+    call system_clock(istart, clock_rate, clock_max)
+    call evolve()
+    call system_clock(istop, clock_rate, clock_max)
+
+    print *, Nstep, " timesteps took ", &
+      real(istop - istart) / real(clock_rate)
+    print *, collisions, " collisions"
 
     ! write result to a file
-    WRITE(file_out,12) 'output.dat', j*Nstep
-    OPEN(unit=1,file=file_out)
-    DO i=1,Nbody
-      WRITE(1,11) mass(i),radius(i),vis(i), &
-        pos(i,Xcoord),pos(i,Ycoord),pos(i,Zcoord), &
-        velo(i,Xcoord),velo(i,Ycoord),velo(i,Zcoord)
-    END DO
-    CLOSE(unit=1)
-  END DO
-  CALL SYSTEM_CLOCK(tstop,clock_rate,clock_max)
+    ! TODO: why format? Try without
+    write(file_out, 12) 'output.dat', j * Nstep
+    open(unit=1, file=file_out)
+    do i = 1, Nbody
+      ! TODO: don't write radius to file, unnecessary
+      write(1,11) mass(i), radius(i), vis(i), &
+        pos (i, Xcoord), pos (i, Ycoord), pos (i, Zcoord), &
+        velo(i, Xcoord), velo(i, Ycoord), velo(i, Zcoord)
+    end do
+    close(unit=1)
+  end do
 
-  WRITE(*,*) (Nsave*Nstep), ' timesteps took ', &
-    REAL(tstop-tstart)/REAL(clock_rate)
+  call system_clock(tstop, clock_rate, clock_max)
+
+  print *, Nsave * Nstep, ' timesteps took ', &
+    real(tstop - tstart) / real(clock_rate)
 
   11 FORMAT(9E16.8)
   12 FORMAT(A10,I3.3)
 
 contains
 
-  SUBROUTINE evolve( count, dt)
-        !include 'coord.inc'
-      INTEGER count, step
-      DOUBLE PRECISION dt
-      DOUBLE PRECISION force
-      DOUBLE PRECISION Size
-      LOGICAL collided
-      INTEGER i,j,k,l
 
-!
-! Loop over timesteps.
-!
-      DO step = 1,count
-        write(*,*) 'timestep ',step
-        write(*,*) 'collisions ',collisions
+  subroutine evolve()
+    double precision :: force, size
+    logical :: collided
+    integer :: step, i, j, k, l
 
-! set the viscosity term in the force calculation
-        DO j=1,Ndim
-          CALL visc_force(Nbody,f(1,j),vis,velo(1,j))
-        END DO
-! add the wind term in the force calculation
-        DO j=1,Ndim
-          CALL wind_force(Nbody,f(1,j),vis,wind(j))
-        END DO
+    do step = 1, Nstep
+      print *, 'timestep ', step
+      print *, 'collisions ', collisions
 
-! calculate distance from central mass
-        DO k=1,Nbody
-          r(k) = 0.0
-        END DO
-        DO i=1,Ndim
-          call add_norm(Nbody,r,pos(1,i))
-        END DO
-        DO k=1,Nbody
-          r(k) = SQRT(r(k))
-        END DO
+      ! set the viscosity term in the force calculation
+      do j = 1, Ndim
+        call visc_force(Nbody, f(1,j), vis, velo(1,j))
+      end do
 
-! calculate central force
+      ! add the wind term in the force calculation
+      do j = 1, Ndim
+        call wind_force(Nbody, f(1,j), vis, wind(j))
+      end do
 
-        DO i=1,Nbody
-          DO l=1,Ndim
-                f(i,l) = f(i,l) - &
-                   calc_force(G*mass(i)*M_central,pos(i,l),r(i))
-          END DO
-        END DO
+      ! calculate distance from central mass
+      do k = 1, Nbody
+        r(k) = 0.0
+      end do
+      do i = 1, Ndim
+        call add_norm(Nbody, r, pos(1,i))
+      end do
+      do k = 1, Nbody
+        r(k) = sqrt(r(k))
+      end do
 
-! calculate pairwise separation of particles
-        k = 1
-        DO i=1,Nbody
-          DO j=i+1,Nbody
-            DO l=1,Ndim
-              delta_pos(k,l) = pos(i,l) - pos(j,l)
-            END DO
+      ! calculate central force
+      do i = 1, Nbody
+        do l = 1, Ndim
+          f(i,l) = f(i,l) &
+            - calc_force(G * mass(i) * M_central, pos(i,l), r(i))
+        end do
+      end do
 
-            k = k + 1
-          END DO
-        END DO
+      ! calculate pairwise separation of particles
+      k = 1
+      do i = 1, Nbody
+        do j = i+1, Nbody
+          do l = 1, Ndim
+            delta_pos(k,l) = pos(i,l) - pos(j,l)
+          end do
+          k = k + 1
+        end do
+      end do
 
-! calculate norm of seperation vector
-        DO k=1,Npair
-          delta_r(k) = 0.0
-        END DO
-        DO i=1,Ndim
-          call add_norm(Npair,delta_r,delta_pos(1,i))
-        END DO
-        DO k=1,Npair
-          delta_r(k) = SQRT(delta_r(k))
-        END DO
+      ! calculate norm of seperation vector
+      do k = 1, Npair
+        delta_r(k) = 0.0
+      end do
+      do i = 1, Ndim
+        call add_norm(Npair, delta_r, delta_pos(1,i))
+      end do
+      do k = 1, Npair
+        delta_r(k) = SQRT(delta_r(k))
+      end do
 
-!
-! add pairwise forces.
-!
-        k = 1
-        DO i=1,Nbody
-          DO j=i+1,Nbody
-            collided=.false.
-            Size = radius(i) + radius(j)
-            DO l=1,Ndim
-!  flip force if close in
-              IF( delta_r(k) .GE. Size ) THEN
-                f(i,l) = f(i,l) - &
-                   calc_force(G*mass(i)*mass(j),delta_pos(k,l),delta_r(k))
-                f(j,l) = f(j,l) + &
-                   calc_force(G*mass(i)*mass(j),delta_pos(k,l),delta_r(k))
-              ELSE
-                f(i,l) = f(i,l) + &
-                    calc_force(G*mass(i)*mass(j),delta_pos(k,l),delta_r(k))
-                f(j,l) = f(j,l) - &
-                    calc_force(G*mass(i)*mass(j),delta_pos(k,l),delta_r(k))
-                collided=.true.
-              END IF
-            END DO
-            IF( collided )THEN
-              collisions = collisions + 1
-            END IF
-            k = k + 1
-          END DO
-        END DO
+      ! add pairwise forces
+      k = 1
+      do i = 1, Nbody
+        do j = i+1, Nbody
+          collided = .false.
+          Size = radius(i) + radius(j)
+          do l = 1, Ndim
 
-! update positions
-        DO i=1,Nbody
-          DO j=1,Ndim
-            pos(i,j) = pos(i,j) + dt * velo(i,j)
-          END DO
-        END DO
+            !  flip force if close in
+            if (delta_r(k) .GE. Size) then
+              f(i,l) = f(i,l) - calc_force( G * mass(i) * mass(j) &
+                                          , delta_pos(k,l) &
+                                          , delta_r(k) )
+              f(j,l) = f(j,l) + calc_force( G * mass(i) * mass(j) &
+                                          , delta_pos(k,l) &
+                                          , delta_r(k) )
+            else
+              f(i,l) = f(i,l) + calc_force( G * mass(i) * mass(j) &
+                                          , delta_pos(k,l) &
+                                          , delta_r(k) )
+              f(j,l) = f(j,l) - calc_force( G * mass(i) * mass(j) &
+                                          , delta_pos(k,l) &
+                                          , delta_r(k) )
+              collided = .true.
+            end if
 
-! update velocities
-        DO i=1,Nbody
-          DO j=1,Ndim
-            velo(i,j) = velo(i,j) + dt * (f(i,j)/mass(i))
-          END DO
-        END DO
+          end do
 
+          if (collided) then
+            collisions = collisions + 1
+          end if
+          k = k + 1
+        end do
+      end do
 
-      END DO
+      ! update positions
+      do i = 1, Nbody
+        do j = 1, Ndim
+          pos(i,j) = pos(i,j) + dt * velo(i,j)
+        end do
+      end do
 
-      END
+      ! update velocities
+      do i = 1, Nbody
+        do j = 1, Ndim
+          velo(i,j) = velo(i,j) + dt * (f(i,j) / mass(i))
+        end do
+      end do
+
+    end do
+  end
 
 
-      SUBROUTINE visc_force(N,f,vis,velo)
-      IMPLICIT NONE
-      INTEGER N, i
-      DOUBLE PRECISION f(N),vis(N),velo(N)
-          DO i=1,N
-            f(i) = -vis(i) * velo(i)
-          END DO
-      END
+  subroutine visc_force(N, f, vis, velo)
+    integer, intent(in) :: N
+    double precision, dimension(N), intent(inout) :: f
+    double precision, dimension(N), intent(in) :: vis, velo
 
-      SUBROUTINE wind_force(N,f,vis,velo)
-      IMPLICIT NONE
-      INTEGER N, i
-      DOUBLE PRECISION f(N),vis(N),velo
-          DO i=1,N
-            f(i) = f(i) -vis(i) * velo
-          END DO
-      END
+    integer :: i
 
-      SUBROUTINE add_norm(N,r,delta)
-      IMPLICIT NONE
-      INTEGER N, k
-      DOUBLE PRECISION r(N),delta(N)
-        DO k=1,N
-          r(k) = r(k) + (delta(k) * delta(k))
-        END DO
-      END
+    do i = 1, N
+      f(i) = -vis(i) * velo(i)
+    end do
+  end
 
-      double precision function calc_force(W,delta,r)
-      IMPLICIT NONE
-      DOUBLE PRECISION :: W,delta,r
-        calc_force=W*delta/(r**3)
-        RETURN
-      END
-END
+
+  subroutine wind_force(N, f, vis, velo)
+    integer, intent(in) ::  N
+    double precision, dimension(N), intent(inout) :: f
+    double precision, dimension(N), intent(in) :: vis
+    double precision, intent(in) :: velo
+
+    integer :: i
+
+    do i = 1, N
+      f(i) = f(i) - vis(i) * velo
+    end do
+  end
+
+
+  subroutine add_norm(N, r, delta)
+    integer, intent(in) :: N
+    double precision, intent(inout) :: r(N)
+    double precision, intent(in) :: delta(N)
+
+    integer :: i
+
+    do i = 1, N
+      r(i) = r(i) + delta(i) * delta(i)
+    end do
+  end
+
+
+  double precision function calc_force(W, delta, r)
+    double precision, intent(in) :: W, delta, r
+
+    calc_force = W * delta / (r**3)
+  end
+end
