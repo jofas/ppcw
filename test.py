2d3eb4e0a3d91ef72268140c27499129bed31d35
jofas <jonas@fc-web.de>
2020-02-23 16:11:59 +0000

    fixed diff unit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 
+
 WARN_LEVEL = 0.001
 ERROR_LEVEL = 0.05
 
@@ -11,41 +12,30 @@ def main():
 
     print("comparing %s with %s:" % (file1, file2))
 
-    d1, d2 = read_data(file1), read_data(file2)
 
-    assert not np.any(np.isnan(d1))
-    assert not np.any(np.isnan(d2))
+    d1, d2 = read_data(file1), read_data(file2)
 
     diff = np.fabs(d1 - d2)
     sum = np.fabs(d1 + d2)
 
     np.where(sum != 0.0, sum, diff / sum)
 
-    r = np.sum(diff, axis=1)
+    mx = max(np.sum(diff, axis=1))
 
-    max = 0.0
-    for x in r:
-      if x > WARN_LEVEL:
-        print("warning!")
-      if x > max:
-        max = x
 
-    if max > ERROR_LEVEL:
-      print("FAILURE")
-    else:
-      print("SUCCESS")
+    print("max = %s" % mx)
 
-    print("max = %s" % max)
+    assert not np.any(np.isnan(d1))
+    assert not np.any(np.isnan(d2))
+    assert mx <= ERROR_LEVEL
+
+    if mx > WARN_LEVEL: print("WARNING!")
 
 
 def read_data(file):
   with open(file) as f:
-    return np.array([to_float(row[2:].replace("  ", " ").split(" "))
-      for row in f.read().split("\n")[:-1]])
-
-
-def to_float(vec_of_str):
-  return [float(x) for x in vec_of_str]
+    return np.array([row.replace("  ", " ").split(" ")[1:]
+      for row in f.read().split("\n")[:-1]], dtype=np.float64)
 
 
 if __name__ == "__main__":
